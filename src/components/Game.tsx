03ff1175f3ba7f922fafbef1776868bdc66f2f60
Gaston Fernandez <gastonf87@gmail.com>
2024-09-11 17:30:15 -0300

    new proyect

---
+++ src/components/Game.tsx
@@ -0,0 +1,5 @@
+const Game: React.FC = () => {
+    return <h1>game</h1>;
+};
+
+export default Game;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // Asegúrate de usar 'react-dom/client' para React 18+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Landing from './components/Landing';
+import Game from './components/Game';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <Router>
+      <Routes>
+        <Route path="/" exact element={<Landing />}></Route>
+        <Route path="/game" exact element={<Game />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
